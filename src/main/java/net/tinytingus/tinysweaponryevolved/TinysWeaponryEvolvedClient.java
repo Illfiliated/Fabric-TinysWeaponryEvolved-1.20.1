637ffc8706dd11aaf66dad7478c82bd2556dcf29
Illfiliated <mromanaj@gmail.com>
2025-09-21 12:49:53 -0500

    Initial commit

---
+++ src/main/java/net/tinytingus/tinysweaponryevolved/TinysWeaponryEvolvedClient.java
@@ -0,0 +1,10 @@
+package net.tinytingus.tinysweaponryevolved;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TinysWeaponryEvolvedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
